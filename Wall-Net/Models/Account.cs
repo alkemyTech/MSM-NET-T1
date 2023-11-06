bd2305634c14f66c5ec0e236016bd1354bafc0b7
RiLeGa <leandroriosga@gmail.com>
2023-11-06 18:22:20 -0300

    creo entidad/repositorios/controllers de Account

---
+++ Wall-Net/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wall_Net.Models
+{
+    public class Account
+    {
+        [Key]
+        public int id { get; set; }
+        public DateTime creationDate { get; set; }
+        public decimal money { get; set; }
+        public bool isBlocked { get; set; }
+        public int user_Id { get; set; }
+        //[ForeignKey("user_Id")]
+        //public virtual Account User { get; set; }
+    }
+}
--- Wall-Net/Program.cs
+++ Wall-Net/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Wall_Net.Repositories;
+using Wall_Net.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddScoped<IAccountsRepository, AccountsRepository>();
+builder.Services.AddScoped<IAccountServices, AccountServices>();
+
 
 var app = builder.Build();
 
