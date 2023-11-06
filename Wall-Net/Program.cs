716054ecc1f13dd7dac268632c84ec3d52a76a91
Leandro-Mumbach <lean21mumbach@gmail.com>
2023-11-06 11:55:54 -0300

    add controllers user

--- Wall-Net/Program.cs
+++ Wall-Net/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Wall_Net.DataAccess;
+using Wall_Net.Repositories;
+using Wall_Net.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add DbContextConf
+builder.Services.AddDbContext<Wall_Net_DbContext>
+    (options => options.UseInMemoryDatabase("WallNetDb"));
+
+// Add Interfaces
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserServices, UserServices>();
 
 var app = builder.Build();
 
---
+++ Wall-Net/Services/IUserServices.cs
@@ -0,0 +1,13 @@
+﻿using Wall_Net.Models;
+
+namespace Wall_Net.Services
+{
+    public interface IUserServices
+    {
+        IEnumerable<User> GetAllUsers();
+        User GetUserById(int id);
+        void AddUser(User user);
+        void UpdateUser(User user);
+        void DeleteUserbyId(int id);
+    }
+}
