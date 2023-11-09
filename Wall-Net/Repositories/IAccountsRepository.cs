1d765ff4e97bd391ea48ea03182f1a5df9876219
Bruno <avilabrunod@gmail.com>
2023-11-09 10:57:35 -0300

    Agrega clases faltantes de Account

---
+++ Wall-Net/Repositories/IAccountsRepository.cs
@@ -0,0 +1,13 @@
+﻿using Wall_Net.Models;
+
+namespace Wall_Net.Repositories
+{
+    public interface IAccountsRepository
+    {
+        Task<IEnumerable<Account>> GetAllAccounts();
+        Task<Account> GetAccountById(int id);
+        Task InsertAccount(Account account);
+        Task UpdateAccount(Account account);
+        Task DeleteAccount(int id);
+    }
+}
