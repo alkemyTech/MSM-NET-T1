543b9a5ad73a53a123aed2d865243b6b96c738bd
Leandro-Mumbach <lean21mumbach@gmail.com>
2023-11-23 10:57:43 -0300

    add list user & delete user front

--- Wall-Net-Front/Pages/Users/ListUsers.cshtml.cs
+++ Wall-Net-Front/Pages/Users/ListUsers.cshtml.cs
@@ -1,12 +1,56 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+using Wall_Net.Models;
 
 namespace Wall_Net_Front.Pages.Users
 {
     public class ListUsersModel : PageModel
     {
-        public void OnGet()
+        public List<User> Users { get; set; }
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPages { get; set; }
+        [TempData]
+        public string Mensaje { get; set; }
+
+        public async Task OnGetAsync(int pageNumber = 1, int pageSize = 10)
         {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync($"http://localhost:5270/api/User?pageNumber={pageNumber}&pageSize={pageSize}");
+                var content = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    Users = JsonConvert.DeserializeObject<List<User>>(content);
+
+                    TotalPages = Users.Count;//(int)Math.Ceiling((double)Users.Count / PageSize)
+                    PageNumber = pageNumber;
+                    PageSize = pageSize;
+                }          
+                else
+                {
+                    Users = new List<User>();
+                }
+            }
+        }
+
+
+        public async Task<IActionResult> OnPostBorrar(int Id)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var response = await httpClient.DeleteAsync($"http://localhost:5270/api/User/{Id}");
+                if (response.IsSuccessStatusCode)
+                {
+                    Mensaje = "Proyecto eliminado correctamente";
+                    return RedirectToPage("ListUsers");
+                }
+                else
+                {
+                    return Page();
+                }
+            }
         }
     }
 }
