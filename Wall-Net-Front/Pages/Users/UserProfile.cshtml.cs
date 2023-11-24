6cac0e7c60f499c2c123961ef38a763110c8b50f
Leandro-Mumbach <lean21mumbach@gmail.com>
2023-11-23 22:52:45 -0300

    add UserProfile User+Account

--- Wall-Net-Front/Pages/Users/UserProfile.cshtml.cs
+++ Wall-Net-Front/Pages/Users/UserProfile.cshtml.cs
@@ -1,12 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Wall_Net.Models;
 
 namespace Wall_Net_Front.Pages.Users
 {
     public class UserProfileModel : PageModel
     {
-        public void OnGet()
+        private readonly HttpClient _httpClient;
+
+        public UserProfileModel()
         {
+            _httpClient = new HttpClient();
+        }
+
+        [BindProperty]
+        public User User { get; set; }
+        public Account Account { get; set; }
+
+        [TempData]
+        public string Mensaje { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int Id = 1)
+        {
+            var userResponse = await _httpClient.GetAsync($"http://localhost:5270/api/User/{Id}");
+            var accountResponse = await _httpClient.GetAsync($"http://localhost:5270/api/Accounts/{Id}");
+
+            if (userResponse.IsSuccessStatusCode && accountResponse.IsSuccessStatusCode)
+            {
+                User = await userResponse.Content.ReadFromJsonAsync<User>();
+                // Leer la respuesta del endpoint de cuentas si es necesario
+                Account = await accountResponse.Content.ReadFromJsonAsync<Account>();
+
+                return Page();
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
--- Wall-Net/Controllers/AccountsController.cs
+++ Wall-Net/Controllers/AccountsController.cs
@@ -18,7 +18,7 @@ namespace Wall_Net.Controllers
             _accountServices = accountServices;
         }
         [HttpGet]
-        [Authorize(Roles = "Admin")]
+        //[Authorize(Roles = "Admin")]
         public async Task<IActionResult> Get(int? numpag)
         {
             var accounts = await _accountServices.GetAll();
@@ -57,7 +57,7 @@ namespace Wall_Net.Controllers
             }
         }
         [HttpGet("{id}")]
-        [Authorize(Roles = "Admin")]
+        //[Authorize(Roles = "Admin")]
         public async Task<IActionResult> Get(int id)
         {
             var account = await _accountServices.GetById(id);
@@ -198,7 +198,7 @@ namespace Wall_Net.Controllers
         }
 
         //Bloqueo de cuenta
-        [Authorize]
+        //[Authorize]
         [HttpPatch("user/block/{id}")]
         public async Task<ActionResult> BlockAccount(int id)
         {
