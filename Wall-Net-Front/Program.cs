6b8c947dd2758f1a50713aa067c2a3b4b2602734
RiLeGa <leandroriosga@gmail.com>
2023-11-24 05:02:28 -0300

    modifico Vista de login

--- Wall-Net-Front/Program.cs
+++ Wall-Net-Front/Program.cs
@@ -3,7 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
+
 var app = builder.Build();
+
+app.UseSession();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
