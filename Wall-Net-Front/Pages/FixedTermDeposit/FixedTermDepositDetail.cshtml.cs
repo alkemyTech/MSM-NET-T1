5e72300fcec7ffc5ce6519beb00c34940be498de
Bruno <avilabrunod@gmail.com>
2023-11-24 14:07:18 -0300

    Leer token desde el sessionStorage

--- Wall-Net-Front/Pages/FixedTermDeposit/FixedTermDepositDetail.cshtml.cs
+++ Wall-Net-Front/Pages/FixedTermDeposit/FixedTermDepositDetail.cshtml.cs
@@ -20,7 +20,7 @@ namespace Wall_Net_Front.Pages.FixedTermDeposit
         {
             using (var httpClient = new HttpClient())
             {
-                var token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJCcnVubyIsImVtYWlsIjoiYnJ1bm9AZW1haWwuY29tIiwiZ2l2ZW5fbmFtZSI6IkJydW5vIiwiZmFtaWx5X25hbWUiOiJBdmlsYSIsInJvbGUiOiJSZWd1bGFyIiwiUG9pbnRzIjoiMTAyMCwwMCIsIklkIjoiMTYiLCJuYmYiOjE3MDA3NTQyOTgsImV4cCI6MTcwMDc1NDg5OCwiaWF0IjoxNzAwNzU0Mjk4LCJpc3MiOiJXYWxsTmV0IiwiYXVkIjoiV2FsbE5ldERldiJ9.ILbHpgmE-boiWZ8yUXgHpE5ZKQxYupHA1D_gt4L-V8O9zlcNpCwmzmDl9XEdEyKgUOlrm3Je1r88-nF5HvM-5g";
+                var token = _httpContextAccessor.HttpContext.Session.GetString("NewSession");
                 httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
                 var response = await httpClient.GetAsync($"http://localhost:5270/api/FixedTermDeposit/{Id}");
 
